ec654a5a53b9bb2e431bfd729dda6029c54a220f
Gizem Turker <gzm.aiesec@gmail.com>
2021-11-09 16:44:40 +0300

    viewcontroller project added

---
+++ 7-ViewController/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by Konpyūtā on 2.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var secondLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
