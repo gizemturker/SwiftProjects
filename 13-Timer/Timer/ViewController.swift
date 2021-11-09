de0b1bc969fb522832d4bfc932a5f3d560bc2548
Gizem Turker <gzm.aiesec@gmail.com>
2021-11-09 17:17:30 +0300

    timer added.

---
+++ 13-Timer/Timer/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Konpyūtā on 8.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var timer = Timer()
+    var kalanZaman = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       kalanZaman = 15
+        
+        label.text = "Zaman: \(kalanZaman)"
+        
+        
+        
+    }
+ 
+    @IBAction func baslatTiklandi(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFonksiyonu), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFonksiyonu() {
+        
+        label.text = "Zaman: \(kalanZaman)"
+        kalanZaman = kalanZaman - 1
+        
+        if kalanZaman == 0 {
+            label.text = "Süre bitti"
+            timer.invalidate()
+        }
+    }
+}
+
+
+
