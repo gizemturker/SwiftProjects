205cf176ad686ebdd73e747cfb9eada44def07ac
Gizem Turker <gzm.aiesec@gmail.com>
2021-11-09 16:38:45 +0300

    variables project added.

---
+++ 2-Variables.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+//string
+
+var name = "Gizem"
+
+var surname = "Turker"
+
+print (name)
+
+name = "yuus"
+
+print(name)
+// kodlar yukarıdan asagı dogru calisir.
+
+var userName = "test"
+var internettenGelenEnYeniVeri = 50 // camelCase !! swift kullanımında bu yazım kullanılır!!
+var internetten_gelen_en_yeni_veri = 40 // snake case
+//integer
+
+let myNumber = 50
+print(myNumber)
+
+userName.uppercased()
+userName.append("1")
+
+myNumber.isMultiple(of: 2)
+
+// 2 nin carpanı mı true verdi!! 50 2 nin katı mı evet o zaman true
+userName.count
+// boolean sadece doğru veya yanlış veri tipini alır
+
+var myBoolean = true
+myBoolean = false
+
+var myOtherNumber = 20
+
+let a = 10.2
+let b = 10.2
+
+a/b
+
+a * b
+// myNumber= 30 yapamayız. bu bir sabit.
+
+let pi = 3.14
+
+//double kesirli sayi
+
+// bu tarz sabitleri değiştirmeye gerek yok. değeri sabitse let koy sabit olarak tut.
+
+
+let myNumber : Int16 = 5
+
+// initializiation (değerini atama, başlatma)
+
+
+
