02b7e9085aebf854e15ad87236ac4ab8af9e0048
Gizem Turker <gzm.aiesec@gmail.com>
2021-11-09 19:37:57 +0300

    citybooks tableview setted.

---
+++ 16-CityBooks/16-CityBooks/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  16-CityBooks
+//
+//  Created by Konpyūtā on 9.11.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var regionNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
---
+++ 16-CityBooks/16-CityBooks/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  16-CityBooks
+//
+//  Created by Konpyūtā on 9.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
