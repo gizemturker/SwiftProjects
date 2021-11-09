3d471ad4a9e295d6cc01b839eddbbd5afae85677
Gizem Turker <gzm.aiesec@gmail.com>
2021-11-09 20:04:00 +0300

    class added.

--- 16-CityBooks/16-CityBooks/ViewController.swift
+++ 16-CityBooks/16-CityBooks/ViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -15,8 +17,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let istanbul = City(name: "İstanbul", region: "Marmara", image: UIImage(named: "istanbul")!)
+        
+    
+        let ankara = City(name: "Ankara", region: "İç Anadolu", image: UIImage(named: "ankara")!)
+        
+        let izmir = City(name: "İzmir", region: "Ege", image: UIImage(named: "izmir")!)
+        
+        let diyarbakir = City(name: "Diyarbakır", region: "Güneydoğu Anadolu", image: UIImage(named: "diyarbakir")!)
+        
+        let antalya = City(name: "Antalya", region: "Akdeniz", image: UIImage(named: "antalya")!)
+        
+        let cityArray = [istanbul,ankara,izmir,diyarbakir,antalya]
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
 }
 
---
+++ 16-CityBooks/16-CityBooks/city.swift
@@ -0,0 +1,25 @@
+//
+//  city.swift
+//  16-CityBooks
+//
+//  Created by Konpyūtā on 9.11.2021.
+//
+
+import Foundation
+import UIKit
+
+
+class City {
+    var name : String
+    var region : String
+    var image : UIImage
+    
+    init(name: String, region: String, image: UIImage) {
+        
+        self.name = name
+        self.region = region
+        self.image = image
+    }
+    
+    
+}
